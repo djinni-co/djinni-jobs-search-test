774a6445aff310b6ebf77ac1943bffa1d94c20be
bohdan <prodanov1540@gmail.com>
2024-08-06 03:13:40 +0300

    Updated jobs_list view and list template to add pagination

--- app/jobs/views.py
+++ app/jobs/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 
 from jobs.models import JobPosting, Company
@@ -12,6 +12,17 @@ def jobs_list(request):
     jobs = JobPosting.objects.all()
     page = int(request.GET.get("page", 1)) or 1
     paginator = Paginator(jobs, JOBS_PER_PAGE)
-    jobs_list = paginator.page(page)
 
-    return render(request, "jobs/list.html", { "jobs": jobs_list })
+    try:
+        jobs_list = paginator.page(page)
+    except PageNotAnInteger:
+        jobs_list = paginator.page(1)
+    except EmptyPage:
+        jobs_list = paginator.page(paginator.num_pages)
+
+    return render(request, "jobs/list.html", {
+        "jobs": jobs_list,
+        "paginator": paginator,
+        "page_obj": jobs_list,
+        "is_paginated": jobs_list.has_other_pages(),
+    })
