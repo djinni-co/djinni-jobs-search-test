8b05bfe6c9965ba6abd03a45e861b9ad7c651e6e
bohdan <prodanov1540@gmail.com>
2024-08-08 02:21:57 +0300

    Use form instead of parsing request; style list.html accordingly

---
+++ app/jobs/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+
+
+class JobFilterForm(forms.Form):
+    q = forms.CharField(
+        required=False,
+        label="All Keywords",
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Search jobs'})
+    )
+    search_position = forms.BooleanField(
+        required=False,
+        label="Search by Position",
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+    )
+    search_long_description = forms.BooleanField(
+        required=False,
+        label="Search by Long Description",
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+    )
+    any_keywords = forms.CharField(
+        required=False,
+        label="Any of Keywords",
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter keywords separated by space'})
+    )
+    exclude_keywords = forms.CharField(
+        required=False,
+        label="Exclude Keywords",
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter keywords separated by space'})
+    )
--- app/jobs/views.py
+++ app/jobs/views.py
@@ -2,33 +2,42 @@ from django.shortcuts import render
 
 from jobs.models import JobPosting
 from jobs.utils import filter_by_query, filter_by_any_keywords, filter_by_exclude_keywords, paginate_jobs
-
+from jobs.forms import JobFilterForm
 import random
 from django.db import transaction
 
 
 def jobs_list(request):
     JOBS_PER_PAGE = 20
+    form = JobFilterForm(request.GET)
 
     jobs = JobPosting.objects.all()
 
-    q = request.GET.get("q", "")
-    search_type = request.GET.get("search_type", "")
-    position_only = request.GET.get("position_only", "") == "on"
-    description_only = request.GET.get("description_only", "") == "on"
-    any_keywords = request.GET.get("any_keywords", "")
-    exclude_keywords = request.GET.get("exclude_keywords", "")
+    if form.is_valid():
+        q = form.cleaned_data.get("q")
+        search_position = form.cleaned_data.get("search_position")
+        search_long_description = form.cleaned_data.get("search_long_description")
+        any_keywords = form.cleaned_data.get("any_keywords")
+        exclude_keywords = form.cleaned_data.get("exclude_keywords")
 
-    jobs = filter_by_query(jobs, q, position_only, description_only)
-    jobs = filter_by_any_keywords(jobs, any_keywords)
-    jobs = filter_by_exclude_keywords(jobs, exclude_keywords)
+        jobs = filter_by_query(jobs, q, search_position, search_long_description)
+        jobs = filter_by_any_keywords(jobs, any_keywords)
+        jobs = filter_by_exclude_keywords(jobs, exclude_keywords)
 
     page = request.GET.get("page", 1)
     jobs_list, paginator = paginate_jobs(jobs, page, JOBS_PER_PAGE)
 
+    # Construct the querystring for pagination links
+    querystring = request.GET.copy()
+    if 'page' in querystring:
+        querystring.pop('page')
+    querystring = querystring.urlencode()
+
     return render(request, "jobs/list.html", {
+        "form": form,
         "jobs": jobs_list,
         "paginator": paginator,
         "page_obj": jobs_list,
         "is_paginated": jobs_list.has_other_pages(),
+        "querystring": querystring,
     })
